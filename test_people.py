f6ad12c7df47dda68cc509ca41d86c1f98c6a875
CharlesNunn001 <cn14@sanger.ac.uk>
2020-03-07 10:44:55 +0000

    Added test for Pink Panther

--- test_people.py
+++ test_people.py
@@ -8,6 +8,11 @@ def test_oliver():
     assert oliver.age == 21
     assert oliver.hobbies == []
 
+def test_pink_panther():
+    p_panther = setup_people()['Pink_Panther']
+    assert p_panther.first_name == 'Pink'
+    assert p_panther.age == 'Unknown'
+    assert p_panther.hobbies == ['Various']
 
 def test_all_keys_match_names():
     for key, person in setup_people().items():
